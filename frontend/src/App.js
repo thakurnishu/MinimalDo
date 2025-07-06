0e21c91dc0f22fc905e22ca8321cc45cce639c12
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-07-06 13:31:59 +0530

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
-const API_URL = process.env.REACT_APP_API_URL;
+import { API_URL } from './utils/env';
 
 function App() {
   const [todos, setTodos] = useState([]);
