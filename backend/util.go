8a0b211c0a4468892a2cd7503de066b629690a18
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-07-18 22:34:20 +0530

    Added trace and log for signoz

--- backend/util.go
+++ backend/util.go
@@ -5,13 +5,6 @@ import (
 	"os"
 )
 
-func errorLog(message string, err error) {
-	if err != nil {
-		slog.Error(message, "error", err)
-		os.Exit(1)
-	}
-}
-
 func GetEnv(key string) string {
 	value := os.Getenv(key)
 	if value == "" {
